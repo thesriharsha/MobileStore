3b5a1e1618f250e9f15515af1d5d6f93c3dd696f
thesriharsha <sri235760@gmail.com>
2023-08-31 10:46:13 +0530

    Third Commit

---
+++ src/main/java/com/mobileshop/entities/Role.java
@@ -0,0 +1,34 @@
+package com.mobileshop.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
